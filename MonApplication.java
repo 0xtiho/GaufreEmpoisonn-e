d1afbfaa9e7db65499feb8bb039360474edf25a6
Racim Ouargli <racimouargli1@gmail.com>
2025-04-18 14:41:54 +0200

    Update MonApplication.java
    ajout poison

--- MonApplication.java
+++ MonApplication.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 
 public class MonApplication extends JComponent {
     int line,column;
-    Image gaufreNormal,gaufreMange;
+    Image gaufreNormal,gaufreMange,poison;
     int [] grille;
     int poisonX,poisonY;
     int widthG,heightG;
     MonApplication(int ligne, int colonne) {
         gaufreNormal= Gaufre.charge("gaufre.png");
         gaufreMange= Gaufre.charge("gaufremange.png");
+        poison= Gaufre.charge("poison.png");
 
         this.line=ligne;
         this.column=colonne;
@@ -22,18 +23,17 @@ public class MonApplication extends JComponent {
         Graphics2D g2d = (Graphics2D) g;
         widthG=getWidth()/column;
         heightG=getHeight()/line;
-        grille[1]=3;
-
+        g2d.drawImage(poison,0,0,widthG,heightG,null);
         for (int j=0;j<column;j++) {
             for (int i=0; i<grille[j]; i++) {
+
                 g2d.drawImage(gaufreMange,j*widthG,getHeight()-((i+1)*heightG),widthG,heightG,null);
             }
             for (int i=0;i<line-grille[j];i++){
-                g2d.drawImage(gaufreNormal,j*widthG,i*heightG,widthG,heightG,null);
-
+                if(!(i==0 && j==0)){
+                    g2d.drawImage(gaufreNormal,j*widthG,i*heightG,widthG,heightG,null);}
             }
         }
-
     }
 
     public static void demarre(JFrame frame, int ligne, int colonne) {
