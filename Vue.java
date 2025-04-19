db16077d21028456d252a1b30bdfad8e06e0d666
Oubraham Maya <Oubraham.Maya@etu.univ-grenoble-alpes.fr>
2025-04-20 00:32:22 +0200

    modif taille grille

--- Vue.java
+++ Vue.java
@@ -23,37 +23,49 @@ public class Vue extends JComponent {
         this.grille=g.getGauffre();
 
     }
+    
     Point pixToCord(Point p){
         return new Point(p.x/(getWidth()/column),p.y/(getHeight()/line));
     }
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
-        widthG=getWidth()/column;
-        heightG=getHeight()/line;
-        g2d.drawImage(poison,0,0,widthG,heightG,null);
-        for (int j=0;j<column;j++) {
-            for (int i=0; i<grille[j]; i++) {
 
-                g2d.drawImage(gaufreMange,j*widthG,getHeight()-((i+1)*heightG),widthG,heightG,null);
-            }
-            for (int i=0;i<line-grille[j];i++){
-                if(!(i==0 && j==0)){
-                    g2d.drawImage(gaufreNormal,j*widthG,i*heightG,widthG,heightG,null);}
-            }
-        }
-        Point po =gauffre.getMouseXY();
-        if (po !=null){
-            Point p = pixToCord(po);
-            System.out.println(p);
-            for (int j=p.x;j<column;j++) {
-                for (int i =p.y; i < line; i++) {
-                    if (i < line - grille[j]){ //juste pour les cases non mangee
+        // Calcul de la taille des cases
+        widthG = getWidth() / column;
+        heightG = getHeight() / line;
 
-                    g2d.drawImage(gaufreSelect, j * widthG, (i * heightG), widthG, heightG, null);}
+        // Dessiner la case empoisonnée (0,0)
+        g2d.drawImage(poison, 0, 0, widthG, heightG, null);
+
+        // Dessiner les cases de la gaufre (mangées et non mangées)
+        for (int j = 0; j < column; j++) {
+            for (int i = 0; i < line; i++) {
+                if (i == 0 && j == 0) {
+                    continue; // Case empoisonnée déjà dessinée
+                }
+                if (i < line - grille[j]) {
+                    // Case non mangée (gaufre normale)
+                    g2d.drawImage(gaufreNormal, j * widthG, i * heightG, widthG, heightG, null);
+                } else {
+                    // Case mangée
+                    g2d.drawImage(gaufreMange, j * widthG, i * heightG, widthG, heightG, null);
                 }
             }
+        }
 
+        // Dessiner les cases sélectionnées
+        Point po = gauffre.getMouseXY();
+        if (po != null) {
+            Point p = pixToCord(po);
+            for (int j = p.x; j < column; j++) {
+                for (int i = p.y; i < line; i++) {
+                    if (i < line - grille[j]) { // Seulement pour les cases non mangées
+                        g2d.drawImage(gaufreSelect, j * widthG, i * heightG, widthG, heightG, null);
+                    }
+                }
+            }
         }
     }
 
