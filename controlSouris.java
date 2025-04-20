8525971eac2e8b4fa6771f8288552b5b59e7dde2
Oubraham Maya <Oubraham.Maya@etu.univ-grenoble-alpes.fr>
2025-04-21 00:22:53 +0200

    IA aleatoire modif

--- controlSouris.java
+++ controlSouris.java
@@ -5,22 +5,33 @@ import java.awt.event.MouseListener;
 public class controlSouris implements MouseListener {
     private Gauffre gauffre;
     private Vue vue;
-    public controlSouris(Gauffre g,Vue v){
+    private Jeu jeu; //dans le but dacceder a la variable tourJoueur
+
+    public controlSouris(Gauffre g,Vue v, Jeu jeu){
         this.gauffre = g;
         this.vue = v;
+        this.jeu = jeu;
     }
-    @Override
-    public void mouseClicked(MouseEvent e){
-        Point p=vue.pixToCord(new Point(e.getX(),e.getY()));
-        if (p.x==0 && p.y==0 ){
-            gauffre.Perdu=1;
-        }
-        gauffre.manger(p.x,p.y);
-        if (!(gauffre.peut_manger(0,1)||gauffre.peut_manger(1,0) )){
-            gauffre.Perdu=1;
-        }
 
-        vue.repaint();
+    @Override
+    public void mouseClicked(MouseEvent e){ //le cic de souris pris en compte ssi humain
+        if (jeu.isTourJoueur()) { //si tour du joueur
+            Point p=vue.pixToCord(new Point(e.getX(),e.getY()));
+            if (p.x==0 && p.y==0 ){
+                gauffre.Perdu=1;
+            }
+            gauffre.manger(p.x,p.y);
+            if (!(gauffre.peut_manger(0,1)||gauffre.peut_manger(1,0) )){
+                gauffre.Perdu=1;
+            }
+
+            vue.repaint();
+            // Passe le tour à l'IA
+            if (gauffre.Perdu == 0) {
+                jeu.setTourJoueur(false);
+                jeu.jouerTourIA();
+            }
+        }
 
     }
     @Override
