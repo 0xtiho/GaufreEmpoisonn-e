5536bf8c4d749d15a4a293c453e3308b6c399007
Boudellal Feth-ellah <feth-ellah.boudellal@etu.univ-grenoble-alpes.fr>
2025-04-22 13:05:53 +0200

    Small fix

--- src/control/controlSouris.java
+++ src/control/controlSouris.java
@@ -21,7 +21,6 @@ public class controlSouris implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e){ //le cic de souris pris en compte ssi humain
-        vue.removeMouseListener(this);
         Point p=vue.pixToCord(new Point(e.getX(),e.getY()));
         if (p.x==0 && p.y==0 ){
             gauffre.Perdu=1;
@@ -36,6 +35,7 @@ public class controlSouris implements MouseListener {
 
         if (ia!=null && gauffre.tour==2) {
             Timer timer = new Timer(1000, evt -> {
+                vue.removeMouseListener(this);
                 ia.jouerCoup();
                 vue.redessine();
                vue.addMouseListener(this);
