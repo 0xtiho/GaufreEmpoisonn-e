820d8121dd392dc92571e6dc4a11f6e00baa95d6
racim <racimouargli1@gmail.com>
2025-04-22 11:28:18 +0200

    modif

--- src/control/controlSouris.java
+++ src/control/controlSouris.java
@@ -2,7 +2,6 @@ package control;
 
 import model.Gauffre;
 import model.IA;
-import model.Jeu;
 import view.Vue;
 import javax.swing.*;
 import java.awt.*;
@@ -22,6 +21,7 @@ public class controlSouris implements MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e){ //le cic de souris pris en compte ssi humain
+        vue.removeMouseListener(this);
         Point p=vue.pixToCord(new Point(e.getX(),e.getY()));
         if (p.x==0 && p.y==0 ){
             gauffre.Perdu=1;
@@ -31,10 +31,14 @@ public class controlSouris implements MouseListener {
 
         gauffre.manger(p.x,p.y,true);
         vue.redessine();
-        if (ia!=null) {
+
+        //desactiver le listener
+
+        if (ia!=null && gauffre.tour==2) {
             Timer timer = new Timer(1000, evt -> {
                 ia.jouerCoup();
                 vue.redessine();
+               vue.addMouseListener(this);
             });
             timer.setRepeats(false);
             timer.start();
